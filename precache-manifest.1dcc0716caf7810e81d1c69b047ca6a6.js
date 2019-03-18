fb4069972d893633e35870e594d65ad58ac653be
avifiqri <myusufavifiqri@gmail.com>
2019-03-18 15:14:46 +0700

    Updates

---
+++ precache-manifest.1dcc0716caf7810e81d1c69b047ca6a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8984b936ee112b57b24f7fed3161dd9",
+    "url": "/home/static/media/buah1.a8984b93.jpeg"
+  },
+  {
+    "revision": "42cf47ad2cbdd6a309fb",
+    "url": "/home/static/css/main.5cfc3740.chunk.css"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "be3c5c1a1cc8fad831cc8eac24a91b4d",
+    "url": "/home/static/media/naga.be3c5c1a.jpeg"
+  },
+  {
+    "revision": "573fb01dd449c8a98dd5",
+    "url": "/home/static/js/2.ffaab1f4.chunk.js"
+  },
+  {
+    "revision": "42cf47ad2cbdd6a309fb",
+    "url": "/home/static/js/main.da214f86.chunk.js"
+  },
+  {
+    "revision": "6ff0b02fec1b1e0484c77d3c3df6d951",
+    "url": "/home/static/media/fruits.6ff0b02f.jpeg"
+  },
+  {
+    "revision": "22e46a4d1e9f10127da303ca720fc533",
+    "url": "/home/static/media/github.22e46a4d.jpeg"
+  },
+  {
+    "revision": "6bab33d7d1fc5a1fe0fd70eee99901a2",
+    "url": "/home/static/media/jeruk.6bab33d7.jpeg"
+  },
+  {
+    "revision": "573fb01dd449c8a98dd5",
+    "url": "/home/static/css/2.6247f935.chunk.css"
+  },
+  {
+    "revision": "1bfd6027c5ef29f1cc41a624ce4be44d",
+    "url": "/home/index.html"
+  }
+];
